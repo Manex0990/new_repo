0d1715053fc6c48fc221f80362e6bec6515a34a6
Manex0990 <168935408+Manex0990@users.noreply.github.com>
2024-12-06 20:31:43 +0300

    main.py

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 import sys
-from PyQt6 import uic
-import io
-from PyQt6.QtCore import QPoint, Qt
+from PyQt6 import QtCore, QtWidgets
+from PyQt6.QtCore import QPointF
 from PyQt6.QtGui import QPainter, QColor
-from PyQt6.QtWidgets import QWidget, QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QWidget
 from random import randint
 
 
-class Painter(QMainWindow):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(600, 400)
+        self.pushButton = QtWidgets.QPushButton(parent=Form)
+        self.pushButton.setGeometry(QtCore.QRect(240, 90, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Нажми сюда"))
+
+
+class Painter(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.paint_ellipse)
 
     def paint_ellipse(self):
         qp = QPainter()
         qp.begin(self)
         rad = randint(20, 100)
-        color = QColor(255, 255, 0)
+        color = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
         qp.setBrush(color)
-        qp.drawEllipse(QPoint(400, 300), rad, rad)
-
-
-def except_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Painter()
-    sys.excepthook = except_hook
-    ex.show()
-    sys.exit(app.exec())
+        qp.drawEllipse(QPointF(300, 200), rad, rad)
+        qp.end()
